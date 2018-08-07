f008caa26524940834f4263feaa0ee05a9b17e9d
Dragneel1234 <Dragneel1234@github.com>
2018-08-07 20:17:25 +0500

    Removed useless definition

--- src/base64_encdec.h
+++ src/base64_encdec.h
@@ -14,7 +14,6 @@
       #define DLL_PUBLIC __declspec(dllimport) // Note: actually gcc seems to also supports this syntax.
     #endif
   #endif
-  #define DLL_LOCAL
 #else
   #if __GNUC__ >= 4
     #ifdef LIBRARY_EXPORT
