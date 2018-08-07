f50eef7f0083d6587cf78c1d4d2fcc75df82efbb
Dragneel1234 <blwal7057@gmail.com>
2018-08-07 15:02:53 +0500

    Linux fix

--- src/base64_encdec.h
+++ src/base64_encdec.h
@@ -6,6 +6,33 @@
 	#define LIBRARY __declspec(dllimport)
 #endif
 
+#if defined _WIN32 || defined __CYGWIN__
+  #ifdef LIBRARY_EXPORT
+    #ifdef __GNUC__
+      #define DLL_PUBLIC __attribute__ ((dllexport))
+    #else
+      #define DLL_PUBLIC __declspec(dllexport) // Note: actually gcc seems to also supports this syntax.
+    #endif
+  #else
+    #ifdef __GNUC__
+      #define DLL_PUBLIC __attribute__ ((dllimport))
+    #else
+      #define DLL_PUBLIC __declspec(dllimport) // Note: actually gcc seems to also supports this syntax.
+    #endif
+  #endif
+  #define DLL_LOCAL
+#else
+  #if __GNUC__ >= 4
+    #ifdef LIBRARY_EXPORT
+        #define DLL_PUBLIC __attribute__ ((visibility ("default")))       
+    #else
+        #define DLL_PUBLIC  __attribute__ ((visibility ("hidden")))
+    #endif
+  #else
+    #define DLL_PUBLIC
+  #endif
+#endif
+
 enum DLL_ERROR
 {
 	BASE64_ISOKAY, BASE64_NOTENOUGHMEMORY, BASE64_SIZEINVALID, 
@@ -22,11 +49,11 @@ extern "C"
 	capacity will then be updated to the size written to output
 	The last \0 should not be added to size in C-style string, espacially in decode
 */
-LIBRARY int32_t base64_encode(int8_t *input, size_t size, int8_t *output, size_t *capacity);
-LIBRARY int32_t base64_decode(int8_t *input, size_t size, int8_t *output, size_t *capacity);
+DLL_PUBLIC int32_t base64_encode(int8_t *input, size_t size, int8_t *output, size_t *capacity);
+DLL_PUBLIC int32_t base64_decode(int8_t *input, size_t size, int8_t *output, size_t *capacity);
 
 /* If message is NULL, message is printed to stdout */
-LIBRARY void base64_geterrormessage(int32_t error_number, int8_t *message, size_t capacity);
+DLL_PUBLIC void base64_geterrormessage(int32_t error_number, int8_t *message, size_t capacity);
 
 
 #ifdef __cplusplus
